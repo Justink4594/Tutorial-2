2cc840c7448e94c6b9308888799b0cde313bbe26
Justink4594 <justink4594@knights.ucf.edu>
2022-10-17 23:40:52 -0400

    First commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c459cebec1e250c3542233818f6f15a82f051938e4a9dd8d797485ad3bdc82f7
+size 451
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29debd28fb8c8534a2896c1d729e9677879a70f5f396504886c17b09edc371d3
+size 1439
